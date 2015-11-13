08e076d9a20592b16636630a9fb5512e8257ad5a
Mark Dane <dane@ohsu.edu>
2015-11-12 18:50:04 -0800

    Added gating on KRT5 and KRT19 and datatables

--- AnalysisWrapper.R
+++ AnalysisWrapper.R
@@ -1,6 +1,6 @@
 library("rmarkdown")
 
-for(cellLine in c("HCC1143", "HCC1954")){
+for(cellLine in c("HCC1143")){
   for(ss in c("SS3")){
     render("MEP-LINCS_Analysis.Rmd", output_file = paste0("Mep-LINCS_Analysis_",cellLine,"_",ss,".html"), output_format = "html_document") 
   }
